e534d3d5aaad7f4a0b7913e781e58f8864009c0d
Patrick Loftus <patrickloftus@roam148-104.ceas.wmich.edu>
2015-03-23 10:07:39 -0400

    pipes

---
+++ Makefile
@@ -0,0 +1,53 @@
+CC=clang
+CFLAGS=-g -O0 -Wall -std=c99
+#add headers to DEPS, otherwise make wont detect changes and recompile
+#when headers change
+DEPS =
+
+# DupeExecDemo
+DED_SRC = DupExecDemo.c makeargv.c error.c
+DED_OBJ = DupExecDemo.o makeargv.o error.o
+DED_EXEC = DupExecDemo 
+
+# PipeExecDemo
+PED_SRC = PipeExecDemo.c error.c
+PED_OBJ = PipeExecDemo.o error.o
+PED_EXEC = PipeExecDemo
+
+# Up
+UP_SRC = Up.c error.c
+UP_OBJ = Up.o error.o
+UP_EXEC = Up
+
+# Shell
+SH_SRC = bash_ba.c makeargv.c error.c
+SH_OBJ = bash_ba.o makeargv.o error.o
+SH_EXEC = bash_ba
+
+#.PHONY declaration just says that 'all' isn't a file name.
+.PHONY: all
+all: $(DED_EXEC) $(PED_EXEC) $(UP_EXEC) $(SH_EXEC)
+
+#compile all .o into executable
+#%.o == all .o files
+#$@ = left side of :
+#$< = right side of :
+%.o: %.c $(DEPS)
+		$(CC) -c -o $@ $< $(CFLAGS)
+
+$(DED_EXEC): $(DED_OBJ) $(DEPS)
+	$(CC) -o $@ $^ $(CFLAGS)
+
+$(PED_EXEC): $(PED_OBJ) $(DEPS)
+	$(CC) -o $@ $^ $(CFLAGS)
+
+$(UP_EXEC): $(UP_OBJ) $(DEPS)
+	$(CC) -o $@ $^ $(CFLAGS)
+
+$(SH_EXEC): $(SH_OBJ) $(DEPS)
+	$(CC) -o $@ $^ $(CFLAGS)
+
+#https://www.gnu.org/software/make/manual/html_node/Cleanup.html#Cleanup
+.PHONY: clean
+clean:
+	find . -maxdepth 1 -type f -executable ! -iname "*.sh" ! -iname "*.c" -exec rm -f {} +
---
+++ bash_ba.c
@@ -0,0 +1,98 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "apue.h"
+#include <sys/wait.h>
+
+/*
+ * Make argv array (*argvp) for tokens in s which are separated by
+ * delimiters.   Return -1 on error or the number of tokens otherwise.
+ */
+int makeargv(char *s, char *delimiters, char ***argvp);
+char *get_line_from_stdin(void)
+{
+  char *line = NULL;
+  size_t bufsize = 0;
+  getline(&line, &bufsize, stdin);
+  return line;
+}
+
+int spawn_child(int my_std_in, int my_std_out, char **args) 
+{
+  pid_t pid, wpid;
+  int status;
+
+  // creates new proc without copying parent's address space
+  pid = vfork();
+
+  if (pid == 0) { //child
+    // make stdin the stdin that was passed 
+    if (my_std_in != 0) {
+      dup2(my_std_in, 0);
+      close(my_std_in);
+    }
+    if (my_std_out != 1) {
+      // make stdout the stdin that was passed 
+      dup2(my_std_out, 1);
+      close(my_std_out);
+    }
+    if (execvp(args[0], args) == -1) {
+      err_sys("bash_ba");
+    }
+  } else if (pid < 0) {
+    err_sys("Failed fork");
+  } 
+  else { //parent
+    do {
+      // wait for child to finish (WUNTRACED returns and reports stopped processes)
+      wpid = waitpid(pid, &status, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status)); //check to see if child status returned
+  }
+
+  // 0 if ok, -1 if error
+  return wpid;
+}
+
+int spawn_children(int num_children, char ***cmds)
+{
+  //backup stdin
+  int stdin_bak = dup(0);
+  int child_std_in = 0;
+  int fd[2];
+
+  for (int child=0; child<num_children-1; child++) {
+    pipe(fd);
+    //fd[1] is now write end of pipe, fd[0] is read end of pipe
+    spawn_child(child_std_in, fd[1], cmds[child]);
+    close(fd[1]); //the child is done writing to the pipe, so we can close the write end
+    child_std_in=fd[0]; //the next childs stdin should be the read end of the pipe that this child wrote
+  }
+  int last_child = num_children-1;
+  if (child_std_in != 0) dup2(child_std_in, 0);
+  spawn_child(child_std_in, 1, cmds[last_child]);
+  dup2(stdin_bak, 0);
+  close(stdin_bak);
+  return 0;
+}
+
+
+int main() 
+{
+  char **children;
+  char *line;
+  int num_children;
+  do {
+    write(STDOUT_FILENO, "\n> ", 3);
+    line = get_line_from_stdin();
+    num_children = makeargv(line, "|\n", &children);
+    char ***cmds = malloc(num_children*sizeof(char *));
+    for (int child=0; child<num_children; child++) {
+      makeargv(children[child], " ", &cmds[child]); 
+    }
+    spawn_children(num_children, cmds);
+    free(cmds);
+  } while (1);
+
+}
+
